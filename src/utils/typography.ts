3865850831c5c3e5f391cacc48e9c1bd6d7248fa
bbawuah <brianbawuah@icloud.com>
2021-01-29 23:08:16 +0100

    .

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,18 +1,18 @@
-
-import Typography from "typography"
+import Typography from 'typography'
 
 const typography = new Typography({
-  baseFontSize: "18px",
+  baseFontSize: '18px',
   baseLineHeight: 1.666,
   headerFontFamily: [
-    "Avenir Next",
-    "Helvetica Neue",
-    "Segoe UI",
-    "Helvetica",
-    "Arial",
-    "sans-serif",
+    'Gilroy-ExtraBold',
+    'Avenir Next',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif'
   ],
-  bodyFontFamily: ["Georgia", "serif"],
+  bodyFontFamily: ['Georgia', 'serif']
 })
 
 export default typography
