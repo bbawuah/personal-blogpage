8b96d82e1409a0a84a10ffd31c082f34a676f0a8
bbawuah <brianbawuah@icloud.com>
2022-01-01 10:14:16 +0100

    remove old projects

--- src/components/Project/Projects.tsx
+++ src/components/Project/Projects.tsx
@@ -13,19 +13,5 @@ export const Projects: React.FC = () => (
         'The Next Gallery is an immersive digital experience that showcases a selection of creatives and talents.'
       }
     />
-    <ProjectListItem
-      url={'https://www.tasksy.work/'}
-      cls={projectsListStyle.listItem}
-      title={'Tasksy - A task management tool for achievers'}
-      desc={'Fullstack taskmanagement web application.'}
-    />
-    <ProjectListItem
-      url={'https://bawuah-chatapp.herokuapp.com/'}
-      cls={projectsListStyle.listItem}
-      title={'Chat app'}
-      desc={
-        'Real time web application where users are able to chat to one another using the Socket library.'
-      }
-    />
   </div>
 )
