fb91ae011b08e98bdffe03afc30d2188b358d3a4
bbawuah <brianbawuah@icloud.com>
2022-01-01 02:55:35 +0100

    changing portfolio back

--- src/components/Project/Projects.tsx
+++ src/components/Project/Projects.tsx
@@ -6,10 +6,12 @@ export const Projects: React.FC = () => (
   <div className={projectsListStyle.list}>
     <h4>Some of my projects</h4>
     <ProjectListItem
-      url={'https://apps.apple.com/in/app/adventist-melodies/id1530974313?ls=1'}
+      url={'https://www.thenext.gallery/'}
       cls={projectsListStyle.listItem}
-      title={'A digital hymnal with sound'}
-      desc={'Fullstack mobile application.'}
+      title={'The Next Gallery'}
+      desc={
+        'The Next Gallery is an immersive digital experience that showcases a selection of creatives and talents.'
+      }
     />
     <ProjectListItem
       url={'https://www.tasksy.work/'}
@@ -18,25 +20,11 @@ export const Projects: React.FC = () => (
       desc={'Fullstack taskmanagement web application.'}
     />
     <ProjectListItem
-      url={'https://hope-foundation.faith/'}
-      cls={projectsListStyle.listItem}
-      title={'H.O.P.E. Foundation'}
-      desc={'Website for the H.O.P.E. foundation.'}
-    />
-    <ProjectListItem
       url={'https://bawuah-chatapp.herokuapp.com/'}
       cls={projectsListStyle.listItem}
       title={'Chat app'}
       desc={
         'Real time web application where users are able to chat to one another using the Socket library.'
-      }
-    />
-    <ProjectListItem
-      url={'https://bawuah-bible-app.herokuapp.com/'}
-      cls={projectsListStyle.listItem}
-      title={'ASV bible app'}
-      desc={
-        'App that renders chapters and/or verses from the American Standard Version of the Bible.'
       }
     />
   </div>
